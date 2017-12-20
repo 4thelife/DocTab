9f93682b1934a30381e43d0ed3d4f214d0116937
César Antonio <c-tonio@hotmail.com>
2017-12-20 15:36:36 -0600

    11
    111

--- app/src/main/java/com/example/antonio/doctab/models/HorarioDeAtencion.java
+++ app/src/main/java/com/example/antonio/doctab/models/HorarioDeAtencion.java
@@ -6,18 +6,7 @@ package com.example.antonio.doctab.models;
 
 public class HorarioDeAtencion {
 
-
-    private String lunes;
-    private String martes;
-    private String miercoles;
-    private String jueves;
-    private String viernes;
-    private String sabado;
-    private String domingo;
-
-
-    // o uso solo:
-    // private String dia;
+    private String dia;
     private String horaEntrada;
     private String horaSalida;
     private String horaInicioComida;
@@ -25,17 +14,12 @@ public class HorarioDeAtencion {
 
     private String firebaseId;
 
+
     public HorarioDeAtencion() {
     }
 
-    public HorarioDeAtencion(String lunes, String martes, String miercoles, String jueves, String viernes, String sabado, String domingo, String horaEntrada, String horaSalida, String horaInicioComida, String horaFinComida, String firebaseId) {
-        this.lunes = lunes;
-        this.martes = martes;
-        this.miercoles = miercoles;
-        this.jueves = jueves;
-        this.viernes = viernes;
-        this.sabado = sabado;
-        this.domingo = domingo;
+    public HorarioDeAtencion(String dia, String horaEntrada, String horaSalida, String horaInicioComida, String horaFinComida, String firebaseId) {
+        this.dia = dia;
         this.horaEntrada = horaEntrada;
         this.horaSalida = horaSalida;
         this.horaInicioComida = horaInicioComida;
@@ -43,60 +27,12 @@ public class HorarioDeAtencion {
         this.firebaseId = firebaseId;
     }
 
-    public String getLunes() {
-        return lunes;
+    public String getDia() {
+        return dia;
     }
 
-    public void setLunes(String lunes) {
-        this.lunes = lunes;
-    }
-
-    public String getMartes() {
-        return martes;
-    }
-
-    public void setMartes(String martes) {
-        this.martes = martes;
-    }
-
-    public String getMiercoles() {
-        return miercoles;
-    }
-
-    public void setMiercoles(String miercoles) {
-        this.miercoles = miercoles;
-    }
-
-    public String getJueves() {
-        return jueves;
-    }
-
-    public void setJueves(String jueves) {
-        this.jueves = jueves;
-    }
-
-    public String getViernes() {
-        return viernes;
-    }
-
-    public void setViernes(String viernes) {
-        this.viernes = viernes;
-    }
-
-    public String getSabado() {
-        return sabado;
-    }
-
-    public void setSabado(String sabado) {
-        this.sabado = sabado;
-    }
-
-    public String getDomingo() {
-        return domingo;
-    }
-
-    public void setDomingo(String domingo) {
-        this.domingo = domingo;
+    public void setDia(String dia) {
+        this.dia = dia;
     }
 
     public String getHoraEntrada() {
