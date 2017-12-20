01fab2eae47bc0a73eb2abac8ffb3c3ed020f7f5
César Antonio <c-tonio@hotmail.com>
2017-12-20 06:46:51 -0600

    <CentroDeTrabajo Incompleto>
    Creando el modelo para el centro de trabajo

---
+++ app/src/main/java/com/example/antonio/doctab/models/CentroDeTrabajo.java
@@ -0,0 +1,28 @@
+package com.example.antonio.doctab.models;
+
+/**
+ * Created by Antonio on 20/12/2017.
+ */
+
+public class CentroDeTrabajo {
+
+
+    private String nombreDelCentro;
+    private String tipoDeCentro;
+    private String telCentro1;
+    private String telCentro2;
+    private String extCentro1;
+    private String extCentro2;
+
+
+
+
+
+    private String firebaseId;
+    private String estatus;
+    private Long fechaDeCreacion;
+    private Long fechaDeEdicion;
+
+
+
+}
