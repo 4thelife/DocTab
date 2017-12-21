090c3778a32fbac926af56a142daf035b3121de2
César Antonio <c-tonio@hotmail.com>
2017-12-21 11:08:33 -0600

    -< fragments

--- app/src/main/java/com/example/antonio/doctab/models/Consultorios.java
+++ app/src/main/java/com/example/antonio/doctab/models/Consultorios.java
@@ -10,8 +10,15 @@ public class Consultorios {
     private String ext1;
     private String tel2;
     private String ext2;
+    private String calle;
+    private String colonia;
+    private String numeroInt;
+    private String numeroExt;
+    private String codPostal;
+    private String ciudad;
+    private String estado;
 
-    private String firebaseIdConsultorio;
+    private String fireBaseId;
     private String estatus;
     private Long fechaDeCreacion;
     private Long fechaDeEdicion;
@@ -20,13 +27,20 @@ public class Consultorios {
     public Consultorios() {
     }
 
-    public Consultorios(String nombreConsultorio, String tel1, String ext1, String tel2, String ext2, String firebaseIdConsultorio, String estatus, Long fechaDeCreacion, Long fechaDeEdicion) {
+    public Consultorios(String nombreConsultorio, String tel1, String ext1, String tel2, String ext2, String calle, String colonia, String numeroInt, String numeroExt, String codPostal, String ciudad, String estado, String fireBaseId, String estatus, Long fechaDeCreacion, Long fechaDeEdicion) {
         this.nombreConsultorio = nombreConsultorio;
         this.tel1 = tel1;
         this.ext1 = ext1;
         this.tel2 = tel2;
         this.ext2 = ext2;
-        this.firebaseIdConsultorio = firebaseIdConsultorio;
+        this.calle = calle;
+        this.colonia = colonia;
+        this.numeroInt = numeroInt;
+        this.numeroExt = numeroExt;
+        this.codPostal = codPostal;
+        this.ciudad = ciudad;
+        this.estado = estado;
+        this.fireBaseId = fireBaseId;
         this.estatus = estatus;
         this.fechaDeCreacion = fechaDeCreacion;
         this.fechaDeEdicion = fechaDeEdicion;
@@ -72,12 +86,68 @@ public class Consultorios {
         this.ext2 = ext2;
     }
 
-    public String getFirebaseIdConsultorio() {
-        return firebaseIdConsultorio;
+    public String getCalle() {
+        return calle;
     }
 
-    public void setFirebaseIdConsultorio(String firebaseIdConsultorio) {
-        this.firebaseIdConsultorio = firebaseIdConsultorio;
+    public void setCalle(String calle) {
+        this.calle = calle;
+    }
+
+    public String getColonia() {
+        return colonia;
+    }
+
+    public void setColonia(String colonia) {
+        this.colonia = colonia;
+    }
+
+    public String getNumeroInt() {
+        return numeroInt;
+    }
+
+    public void setNumeroInt(String numeroInt) {
+        this.numeroInt = numeroInt;
+    }
+
+    public String getNumeroExt() {
+        return numeroExt;
+    }
+
+    public void setNumeroExt(String numeroExt) {
+        this.numeroExt = numeroExt;
+    }
+
+    public String getCodPostal() {
+        return codPostal;
+    }
+
+    public void setCodPostal(String codPostal) {
+        this.codPostal = codPostal;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getFireBaseId() {
+        return fireBaseId;
+    }
+
+    public void setFireBaseId(String fireBaseId) {
+        this.fireBaseId = fireBaseId;
     }
 
     public String getEstatus() {
