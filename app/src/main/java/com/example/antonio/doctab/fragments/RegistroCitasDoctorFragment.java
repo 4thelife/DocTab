c789b061c8053e89f64cd83fda5db0e93e6e32f9
Ricardo Zapata <36306229+RichZapata@users.noreply.github.com>
2018-02-21 13:10:24 -0600

    RegistroCitasLleno

--- app/src/main/java/com/example/antonio/doctab/fragments/RegistroCitasDoctorFragment.java
+++ app/src/main/java/com/example/antonio/doctab/fragments/RegistroCitasDoctorFragment.java
@@ -10,34 +10,59 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.antonio.doctab.R;
+import com.example.antonio.doctab.Utils.Constants;
+import com.example.antonio.doctab.fragments.interfaces.MainRegisterInterface;
+import com.example.antonio.doctab.helpers.DecodeExtraHelper;
+import com.example.antonio.doctab.models.Usuarios;
+
 /**
  * Created by Ricardo on 13/02/2018.
  */
 
-public class RegistroCitasDoctorFragment extends Fragment implements View.OnClickListener{
+public class RegistroCitasDoctorFragment extends Fragment {
+
+    private MainRegisterInterface activityInterface;
+
+    private static DecodeExtraHelper _MAIN_DECODE;
+    private static Usuarios _SESSION_USER;
 
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.fragment_registro_citas_doctor, container, false);
+
+        _MAIN_DECODE = (DecodeExtraHelper) getActivity().getIntent().getExtras().getSerializable(Constants.KEY_MAIN_DECODE);
+        _SESSION_USER = (Usuarios) getActivity().getIntent().getExtras().getSerializable(Constants.KEY_SESSION_USER);
+
+        FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+        FragmentTransaction mainFragment = fragmentManager.beginTransaction();
+
+        mainFragment.replace(R.id.fragment_registro_citas_doctor_container, new FormularioCitasDoctorFragment(), Constants.FRAGMENT_FORMULARIO_CITAS_DOCTOR);
+
+        mainFragment.commit();
+
+        return view;
+
     }
 
-    @Override
-    public void onStart() {
-        super.onStart();
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
     }
 
 
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
+        try {
+            activityInterface=(MainRegisterInterface)getActivity();
+        }catch (ClassCastException e){
+            throw new ClassCastException(getActivity().toString()+"debe implementar");
+        }
     }
 
-    @Override
-    public void onClick(View view) {
 
-    }
 
 
 }
