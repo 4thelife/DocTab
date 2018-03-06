0148d02328362374ef2eb63a28aea6e99f4c99d8
César Antonio <c-tonio@hotmail.com>
2018-03-06 12:46:10 -0600

    <

--- app/src/main/java/com/example/antonio/doctab/fragments/FormularioCitasFragment.java
+++ app/src/main/java/com/example/antonio/doctab/fragments/FormularioCitasFragment.java
@@ -8,8 +8,10 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 import com.example.antonio.doctab.R;
 
@@ -23,10 +25,11 @@ public class FormularioCitasFragment extends Fragment implements View.OnClickLis
 
 
     private static TextInputLayout tilCitasFecha;
-
+    Spinner sp_hora,sp_minuto;
     Calendar currentDate;
     EditText fecha;
     int dia, mes, year;
+
 
 
     @Override
@@ -41,7 +44,13 @@ public class FormularioCitasFragment extends Fragment implements View.OnClickLis
         year = currentDate.get(Calendar.YEAR);
         tilCitasFecha.getEditText().setText(dia+"/"+mes+"/"+year);
 
+        sp_hora = (Spinner)view.findViewById(R.id.sp_hora);
+        sp_minuto = (Spinner)view.findViewById(R.id.sp_minutos);
+
         fecha.setOnClickListener(this);
+
+
+
 
         return view;
     }
