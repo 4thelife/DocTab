001393b77ca222cc456a005852bfa37b6ace65fb
Ricardo Zapata <36306229+RichZapata@users.noreply.github.com>
2018-03-10 13:59:31 -0600

    TimePickerCita
    Se agrego un timepicker al formulario de la cita

--- app/src/main/java/com/example/antonio/doctab/fragments/FormularioCitasFragment.java
+++ app/src/main/java/com/example/antonio/doctab/fragments/FormularioCitasFragment.java
@@ -1,6 +1,7 @@
 package com.example.antonio.doctab.fragments;
 
 import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
@@ -12,6 +13,7 @@ import android.widget.ArrayAdapter;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TimePicker;
 
 import com.example.antonio.doctab.R;
 
@@ -24,11 +26,11 @@ import java.util.Calendar;
 public class FormularioCitasFragment extends Fragment implements View.OnClickListener {
 
 
-    private static TextInputLayout tilCitasFecha;
+    private static TextInputLayout tilCitasFecha, tilCitasHora;
     Spinner sp_hora,sp_minuto;
-    Calendar currentDate;
-    EditText fecha;
-    int dia, mes, year;
+    Calendar currentDate,currentTime;
+    EditText fecha,hora;
+    int dia, mes, year,hour, minuto;
 
 
 
@@ -36,7 +38,11 @@ public class FormularioCitasFragment extends Fragment implements View.OnClickLis
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_formulario_citas,container,false);
 
+        /**
+         * Seleccionar la fecha
+         */
         tilCitasFecha = (TextInputLayout) view.findViewById(R.id.til_citas_fecha);
+
         fecha = (EditText)view.findViewById(R.id.ed_citas_fecha);
         currentDate = Calendar.getInstance();
         dia = currentDate.get(Calendar.DAY_OF_MONTH);
@@ -49,6 +55,17 @@ public class FormularioCitasFragment extends Fragment implements View.OnClickLis
 
         fecha.setOnClickListener(this);
 
+        /**
+         * Seleccionar la hora
+         */
+        tilCitasHora = (TextInputLayout) view.findViewById(R.id.til_citas_hora);
+        hora = (EditText)view.findViewById(R.id.ed_citas_hora);
+        currentTime = Calendar.getInstance();
+        hour = currentTime.get(Calendar.HOUR_OF_DAY);
+        minuto = currentTime.get(Calendar.MINUTE);
+
+        tilCitasHora.getEditText().setText(hora+":"+minuto);
+        hora.setOnClickListener(this);
 
 
 
@@ -83,6 +100,15 @@ public class FormularioCitasFragment extends Fragment implements View.OnClickLis
                             }
                         },year,mes,dia);
                 datePickerDialog.show();
+                break;
+            case R.id.ed_citas_hora:
+                TimePickerDialog timePickerDialog = new TimePickerDialog(getContext(), new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                        tilCitasHora.getEditText().setText(hourOfDay+":"+minute);
+                    }
+                },hour,minuto,true);
+                timePickerDialog.show();
                 break;
         }
 
