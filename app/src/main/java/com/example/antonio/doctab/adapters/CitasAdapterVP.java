c7a66d9051999bc229a2c0368731286a17179b97
Ricardo Zapata <36306229+RichZapata@users.noreply.github.com>
2018-03-20 10:03:47 -0600

    aun no termino
    Cesar, tu no te la comes, tu te la tragas.

---
+++ app/src/main/java/com/example/antonio/doctab/adapters/CitasAdapterVP.java
@@ -0,0 +1,91 @@
+package com.example.antonio.doctab.adapters;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.antonio.doctab.R;
+import com.example.antonio.doctab.fragments.CitasFragment;
+import com.example.antonio.doctab.helpers.DecodeItemHelper;
+import com.example.antonio.doctab.models.Citas;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Ricardo on 13/02/2018.
+ */
+
+public class CitasAdapterVP extends RecyclerView.Adapter<CitasAdapterVP.ViewHolder> {
+
+    View.OnClickListener onClickListener;
+    List<Citas> dataList = new ArrayList<>();
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView txtNombre;
+        TextView txtFecha;
+        TextView txtHora;
+        TextView txtAsunto;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            txtNombre = (TextView) itemView.findViewById(R.id.item_citas_nombre_nbtn);
+            txtFecha = (TextView) itemView.findViewById(R.id.item_citas_fecha_nbtn);
+            txtHora = (TextView) itemView.findViewById(R.id.item_citas_horario_nbtn);
+            txtAsunto = (TextView) itemView.findViewById(R.id.item_citas_asunto_nbtn);
+
+        }
+
+    }
+
+    public void setOnClickListener(View.OnClickListener onClickListener) {
+        this.onClickListener = onClickListener;
+    }
+
+    public Citas getItemByPosition(int position) {
+        return dataList.get(position);
+    }
+
+    public void addAll(List<Citas> _data) {
+        this.dataList.addAll(_data);
+    }
+
+    public void remove(int position) {
+        this.dataList.remove(position);
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.fragment_item_citas_nbtn, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        final Citas item = dataList.get(position);
+        /**Llena el objeto que sera enviado al fragmento**/
+        final DecodeItemHelper decodeItem = new DecodeItemHelper();
+
+        decodeItem.setItemModel(item);
+        decodeItem.setPosition(position);
+
+        holder.txtNombre.setText("Dr Fulanito Hernandez");
+        holder.txtFecha.setText(item.getFecha());
+        holder.txtHora.setText(item.getHora());
+        holder.txtAsunto.setText(item.getAsunto());
+
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return dataList == null ? 0 : dataList.size();
+    }
+
+}
--- app/src/main/java/com/example/antonio/doctab/fragments/InicioFragment.java
+++ app/src/main/java/com/example/antonio/doctab/fragments/InicioFragment.java
@@ -4,19 +4,25 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.example.antonio.doctab.R;
 import com.example.antonio.doctab.Utils.Constants;
+import com.example.antonio.doctab.adapters.CitasAdapter;
 import com.example.antonio.doctab.adapters.DoctoresAdapter;
 import com.example.antonio.doctab.fragments.interfaces.NavigationDrawerInterface;
+import com.example.antonio.doctab.models.Citas;
 import com.example.antonio.doctab.models.Usuarios;
 import com.example.antonio.doctab.services.SharedPreferencesService;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.List;
 
 /**
  * Created by GIGABYTE on 06/03/2018.
@@ -28,23 +34,44 @@ public class InicioFragment extends Fragment {
 
     private static NavigationDrawerInterface activityInterface;
     public static LinearLayout linearLayout;
-    private static DoctoresAdapter adapter;
+
+    /**
+     * Declaracion de los elementos de la vista
+     */
+    private static List<Citas> dataList;
+    private static RecyclerView recyclerView;
+    public static CitasAdapter adapter;
+    TextView vista;
     /**
      * Declaraciones de firebase
      **/
     private FirebaseDatabase database;
-    private DatabaseReference drDoctores;
+    private DatabaseReference drCitas;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_inicio, container, false);
 
+        vista = (TextView) view.findViewById(R.id.txt_inicio);
+
         _SESSION_USER = SharedPreferencesService.getUsuarioActual(getContext());
+        linearLayout = (LinearLayout) view.findViewById(R.id.view_no_resultados);
+        recyclerView = (RecyclerView) view.findViewById(R.id.recycler_view_citas);
+
         database = FirebaseDatabase.getInstance();
-        //Se crea la instancia para acceder a la consulta
-        drDoctores = database.getReference(Constants.FB_KEY_MAIN_DOCTORES)
-                .child(_SESSION_USER.getFirebaseId());
+        switch (_SESSION_USER.getTipoDeUsuario()){
+            case Constants.FB_KEY_ITEM_TIPO_USUARIO_PACIENTE:
+                drCitas = database.getReference(Constants.FB_KEY_MAIN_CITAS);
+                vista.setText("Inicio en modo paciente");
+                break;
+            case Constants.FB_KEY_ITEM_TIPO_USUARIO_DOCTOR:
+                drCitas = database.getReference(Constants.FB_KEY_MAIN_CITAS)
+                        .child(_SESSION_USER.getFirebaseId());
+                vista.setText("Inicio en modo doctor");
+                break;
+        }
+
 
         return view;
     }
@@ -62,6 +89,23 @@ public class InicioFragment extends Fragment {
     @Override
     public void onStart() {
         super.onStart();
+        switch (_SESSION_USER.getTipoDeUsuario()){
+            case Constants.FB_KEY_ITEM_TIPO_USUARIO_DOCTOR:
+                listadoInicioDoctor();
+                break;
+            case Constants.FB_KEY_ITEM_TIPO_USUARIO_PACIENTE:
+                listadoInicioPaciente();
+                break;
+        }
+
+    }
+
+    private void listadoInicioDoctor() {
+
+    }
+
+    private void listadoInicioPaciente() {
+
     }
 
     @Override
